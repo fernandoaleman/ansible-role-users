152c08975795b215f6a51e19d3968c5b23c4add2
Fernando Alemán <fernandoaleman@mac.com>
2025-05-04 18:04:46 -0500

    Add users

--- molecule/default/tests/conftest.py
+++ molecule/default/tests/conftest.py
@@ -1,1 +1,31 @@
 # molecule/default/tests/conftest.py
+import pytest
+
+USERS_ADD = [
+    {"name": "deploy"},
+    {"name": "devops", "groups": ["sudo"]},
+    "foo",
+]
+
+
+@pytest.fixture
+def test_users_add():
+    normalized = []
+
+    for user in USERS_ADD:
+        if isinstance(user, str):
+            name = user
+            user = {"name": name}
+        else:
+            name = user["name"]
+
+        normalized.append(
+            {
+                "name": name,
+                "home": user.get("home", f"/home/{name}"),
+                "shell": user.get("shell", "/bin/bash"),
+                "groups": user.get("groups", []),
+            }
+        )
+
+    return normalized
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,7 +1,14 @@
 # molecule/default/tests/test_default.py
 
 
-def test_host_is_reachable(host):
-    etc = host.file("/etc")
-    assert etc.exists
-    assert etc.is_directory
+def test_user_exists_and_configured(host, test_users_add):
+    for user in test_users_add:
+        u = host.user(user["name"])
+
+        assert u.exists
+        assert u.name == user["name"]
+        assert u.home == user["home"]
+        assert u.shell == user["shell"]
+
+        for group in user["groups"]:
+            assert group in u.groups
